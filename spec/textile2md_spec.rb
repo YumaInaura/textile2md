80687715384072f4da46e3ae81e165fdaae66d26
Yumainaura <yuma.inaura@gmail.com>
2018-07-18 18:19:37 +0900

    Add spec Textile2md.convert

--- spec/textile2md_spec.rb
+++ spec/textile2md_spec.rb
@@ -3,7 +3,26 @@ RSpec.describe Textile2md do
     expect(Textile2md::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+
+  describe '.convert' do
+    subject { Textile2md.convert(textile) }
+
+    let(:textile) do
+      <<~TEXT
+        h1. header1
+        
+        * list
+        * list
+        * list
+        
+        h2. header2
+      TEXT
+    end
+
+    it "converts textile to markdown" do
+      is_expected.to include '# header1'
+      is_expected.to include '# header2'
+      is_expected.to include '- list'
+    end
   end
 end
